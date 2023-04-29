5e7a3bb9b1cf6d3d37f6d6c25dce63b860d2c033
Console / ريدوي <92029487+h4x3r1@users.noreply.github.com>
2023-04-29 10:34:27 +0600

    Update Hax4_WebX.sh

--- Hax4_WebX.sh
+++ Hax4_WebX.sh
@@ -1,7 +1,3 @@
 # Obfuscated with au0_root 📑
 # #If You Wanna Take Credits For This Code, Please Look Yourself Again.
 # https://www.github.com/h4x3r1
-#!/bin/bash
-
-God="BlessYou";krF=FoL;Gnr=Vl;QIy=Is;UOt=FC;kVE=Sk;KlM=gM;Xwe=T>
-";DSi=WYX;LHA=Gfx;VgU=gJZ;Rxh=xYy;bXG=gvs;eLoP="e";u18="6";Yt=">
