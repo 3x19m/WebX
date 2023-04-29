89fcf0863710c6644c797505164f215719ea5b8d
Console / ريدوي <92029487+h4x3r1@users.noreply.github.com>
2023-04-29 10:31:26 +0600

    Update Hax4_WebX.sh

--- Hax4_WebX.sh
+++ Hax4_WebX.sh
@@ -2,5 +2,6 @@
 # #If You Wanna Take Credits For This Code, Please Look Yourself Again.
 # https://www.github.com/h4x3r1
 #!/bin/bash
+
 God="BlessYou";krF=FoL;Gnr=Vl;QIy=Is;UOt=FC;kVE=Sk;KlM=gM;Xwe=T>
 ";DSi=WYX;LHA=Gfx;VgU=gJZ;Rxh=xYy;bXG=gvs;eLoP="e";u18="6";Yt=">
